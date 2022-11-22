22a0da26e320eca23f5682faa72d11286537c80c
Jeff Lee <jeff.lee@redcypress.com.tw>
2022-11-22 09:02:43 +0800

    MyFirstCypress.spec.js and navigation.spec.js

---
+++ cypress/integration/MyFirstCypress.spec.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+describe("Navigation", () => {
+    it("should navigate to conference sessions page",() => {
+        cy.visit("https://www.msn.com/zh-tw");        
+    });
+})
